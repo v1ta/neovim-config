119f85df2607e2db7dcc435f6504138ecfb1934e
Joseph DeVita <jodvita@gmail.com>
2022-04-23 20:14:06 -0700

    add LSP support

---
+++ lua/config/lsp/lsp-installer.lua
@@ -0,0 +1,30 @@
+local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+if not status_ok then
+  print("ERROR: plugin: nvim-lsp-installer failed to load")
+	return
+end
+
+-- Register a handler that will be called for all installed servers.
+-- Alternatively, you may also register handlers on specific server instances instead (see example below).
+lsp_installer.on_server_ready(function(server)
+	local opts = {
+		on_attach = require("config.lsp.handlers").on_attach,
+		capabilities = require("config.lsp.handlers").capabilities,
+	}
+
+   -- for specfic LS, pass extra options here --
+	 if server.name == "jsonls" then
+	 	local jsonls_opts = require("config.lsp.settings.jsonls")
+	 	opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+	 end
+
+	 if server.name == "sumneko_lua" then
+	 	local sumneko_opts = require("config.lsp.settings.sumneko_lua")
+	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+	 end
+
+	-- This setup() function is exactly the same as lspconfig's setup function.
+	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+	server:setup(opts)
+end)
+
